f2381335f07cee3b6a8307c15836aa7794f45f8d
Maxym1507 <plast7575@gmail.com>
2023-04-26 13:04:57 +0300

    Update SOCKETS_CLIENT.cpp

--- SOCKETS_CLIENT.cpp
+++ SOCKETS_CLIENT.cpp
@@ -8,13 +8,13 @@ using namespace std;
 
 int main()
 {
-	WSADATA wsData;
-	WORD ver = MAKEWORD(2, 2);
-	int wsOk = WSAStartup(ver, &wsData);
-	if (wsOk != 0) {
-		cerr << "Can't Initialize winsock! Quitting" << endl;
-		return 1;
-	}
+    WSADATA wsData;
+    WORD ver = MAKEWORD(2, 2);
+    int wsOk = WSAStartup(ver, &wsData);
+    if (wsOk != 0) {
+        cerr << "Can't Initialize winsock! Quitting" << endl;
+        return 1;
+    }
     SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (clientSocket == INVALID_SOCKET) {
         cerr << "Can't create socket! Quitting" << endl;
@@ -35,25 +35,34 @@ int main()
         return 1;
     }
 
-    char buf[4096];
+    char buf[1000000];
     string userInput;
 
     double bytesSent = 0.0;
     double totalSeconds = 0.0;
-
+    int i = 0;
     do {
         cout << "> ";
-        getline(cin, userInput);
+        //getline(cin, userInput);
+        userInput = "";
+        if (i < 10)
+            for (int i = 0; i < 99000; i++) {
+                userInput += "qwertyuiop";
+            }
+        else
+            userInput = "";
+        i++;
 
         if (userInput.size() > 0) {
             auto start = chrono::high_resolution_clock::now();
 
             int sendResult = send(clientSocket, userInput.c_str(), userInput.size() + 1, 0);
+            auto end = chrono::high_resolution_clock::now();
             if (sendResult != SOCKET_ERROR) {
-                ZeroMemory(buf, 4096);
-                int bytesReceived = recv(clientSocket, buf, 4096, 0);
+                ZeroMemory(buf, 1000000);
+                int bytesReceived = recv(clientSocket, buf, 1000000, 0);
                 if (bytesReceived > 0) {
-                    auto end = chrono::high_resolution_clock::now();
+                    
                     auto diff = end - start;
                     double seconds = chrono::duration_cast<chrono::microseconds>(diff).count() / 1000000.0;
 
